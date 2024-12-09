9dd56c175d03b45a29015800c02553b2e139b67e
NIkhil Kewat <kewatnikhil@gmail.com>
2024-12-09 15:02:48 +0530

    Test 2
    Return the same number if the input has only one number

--- src/classes/calculator.test.ts
+++ src/classes/calculator.test.ts
@@ -2,8 +2,15 @@ import { Calculator } from "./calculator";
 
 describe("Incubyte TDD Assesment", () => {
 
+    const calculator = new Calculator();
+
     test("should return 0 for an empty string", () => {
-        const calculator = new Calculator();
         expect(calculator.calculate("")).toBe(0);
-    })
+    });
+
+    test("should return the number itself if there is only one  number", () => {
+        const calculator = new Calculator();
+        expect(calculator.calculate("1")).toBe(1);
+    });
+
 })
--- src/classes/calculator.ts
+++ src/classes/calculator.ts
@@ -9,7 +9,9 @@ export class Calculator {
     private operation: Operation;
     private allowNegative: boolean;
     //default delimiter
-    private delimiterRegex = /,|\n/;
+    private delimiterRegex = /^\/\/(\[.*?\]|.)\n/;
+
+
 
     constructor(options: Options = { operation: "add", allowNegative: false }) {
         this.operation = options.operation;
@@ -20,6 +22,33 @@ export class Calculator {
     calculate(values: string): number {
 
         if (!values) return 0;
-        return 0;
+
+        const  numbers  = this.parseValues(values);
+
+        if (numbers.length === 1) return numbers[0];
+
+        
+
+        return 0
+    }
+
+    private parseValues(value: string):  number[]  {
+        let delimiter = /,|\n/;
+        let numberString = value;
+
+        if (value.startsWith("//")) {
+            const match = value.match(/^\/\/(.+)\n/);
+            if (match) {
+                delimiter = new RegExp(`[${match[1]}]`);
+                numberString = value.split("\n").slice(1).join("\n");
+            }
+        }
+
+        const numbers = numberString
+            .split(delimiter)
+            .map((num) => parseInt(num.trim(), 10))
+            .filter((num) => !isNaN(num));
+
+        return numbers;
     }
 }
